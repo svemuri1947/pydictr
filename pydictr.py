87c2aede59ef00363f1ba12ccc6019afa1979cca
dagans <dagans@ford.com>
2016-09-30 14:34:50 -0400

    added correct library for http rest

--- pydictr.py
+++ pydictr.py
@@ -1,17 +1,25 @@
 import json
-from urllib.request import urlopen
+import urllib.request
 
+api_url = 'https://owlbot.info/api/v1/dictionary/'
+api_suffix = '?format=json'
+
+# lookup that word
 def lookup( dictword ):
   dictword = dictword.rstrip()
   if check_alpha( dictword ):
-    print('it is all alpha')
+    return dictword
+  else:
+    return 'error'
 
+# check that it's all alpha
 def check_alpha( dictword ):
   if dictword.isalpha():
     return True
   else:
     return False  
 
+# Get user input for word
 def get_word():
   word_input = input("Please enter a word in English: ")
   return word_input
