5c18c53250e7ce3179790a449a297cdd5fb4cdc7
dagans <dagans@ford.com>
2016-09-30 14:09:57 -0400

    nothing works

--- pydictr.py
+++ pydictr.py
@@ -1,3 +1,13 @@
 import json
-import urllib2
-json.load(urllib2.urlopen("url"))
+from urllib.request import urlopen
+
+def lookup( dictword ):
+  return dictword
+
+def get_word():
+  word_input = input("Please enter a word in English: ")
+  return word_input
+
+str(lookup(get_word))
+
+# json.load(urllib2.urlopen("url"))
